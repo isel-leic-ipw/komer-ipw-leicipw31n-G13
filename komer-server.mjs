e4ecb43f4c923be1aafe1ad6c39aa21292c2a336
TiagoNeves9 <tiagonevesx9@gmail.com>
2022-05-11 08:08:13 +0100

    Update on komer-server file, testing

--- komer-server.mjs
+++ komer-server.mjs
@@ -1,16 +1,37 @@
 import express from 'express'
 const app = express()
-const PORT = 1904
-import gamesApi from './games-api.mjs'
+const PORT = 8080
 app.use(express.json())
 
 
 
-// Configure CRUD routes to manage jokes 
+/* // Configure CRUD routes to manage jokes 
 app.get('/api/jokes', gamesApi.getGames)           // Get all jokes
 app.get('/api/jokes/:id', gamesApi.getGame)        // Get a joke details
 app.delete('/api/jokes/:id', gamesApi.deleteGame)  // Delete a joke
 app.put('/api/jokes/:id', gamesApi.updateGame)     // Update a joke
-app.post('/api/jokes', gamesApi.createGame)        // Delete a joke
+app.post('/api/jokes', gamesApi.createGame)        // Delete a joke */
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
+
+app.get('/tshirt', (req,res) => {
+        res.status(200).send({
+            tshirt: '👕',
+            size: 'large'
+        });
+});
+
+app.post('/tshirt/:id', (req,res)=>{
+
+      const {id} = req.params;
+      const {logo} = req.body;
+      
+      if(!logo){
+          res.status(418).send({message : "Logo needded!"})
+      }
+      
+      res.send({
+          tshirt: `👕 with your ${logo} and ID of ${id}`,
+      });
+
+});
---
+++ komer-services.mjs
@@ -0,0 +1,11 @@
+/*
+    modulo services é fundamental, é onde é implementada a logica da API 
+    manda gravar , mandar obter, manda aceder aos dados 
+
+    implement data access to memory storage 
+
+*/
+
+
+//import apidata  // ficheiro q é preciso fazer 
+
