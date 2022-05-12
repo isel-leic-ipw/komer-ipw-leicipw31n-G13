e329fc07281180ec0ef800ff6bb927f00ca187ae
TiagoNeves9 <tiagonevesx9@gmail.com>
2022-05-12 17:36:55 +0100

    Update of Komer-server and Komer-web-api files

--- komer-server.mjs
+++ komer-server.mjs
@@ -3,35 +3,13 @@ const app = express()
 const PORT = 8080
 app.use(express.json())
 
-
-
-/* // Configure CRUD routes to manage jokes 
-app.get('/api/jokes', gamesApi.getGames)           // Get all jokes
-app.get('/api/jokes/:id', gamesApi.getGame)        // Get a joke details
-app.delete('/api/jokes/:id', gamesApi.deleteGame)  // Delete a joke
-app.put('/api/jokes/:id', gamesApi.updateGame)     // Update a joke
-app.post('/api/jokes', gamesApi.createGame)        // Delete a joke */
+import recipesApi from './komer-web-api.mjs'
+app.use(recipesApi)
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
 
-app.get('/tshirt', (req,res) => {
-        res.status(200).send({
-            tshirt: '👕',
-            size: 'large'
-        });
-});
 
-app.post('/tshirt/:id', (req,res)=>{
-
-      const {id} = req.params;
-      const {logo} = req.body;
-      
-      if(!logo){
-          res.status(418).send({message : "Logo needded!"})
-      }
-      
-      res.send({
-          tshirt: `👕 with your ${logo} and ID of ${id}`,
-      });
-
-});
+/**
+ * This file is responsible for being the entry point into the api
+ * 
+ */
--- komer-web-api.mjs
+++ komer-web-api.mjs
@@ -1,36 +1,17 @@
 // File rsponsibilities
 // Have the functions that handle HTTP requests
 
-export default {
-    getGames: getGames,
-    getGame: getGame,
-    createGame: createGame,
-    deleteGame: deleteGame,
-    updateGame: updateGame,
-}
+import express from 'express'
+import services from './komer-services.mjs'
 
+const app = express.Router()
+export default recipesApi
 
-function getGames(req, resp){
-    resp.json({message : "Get Games" })
-}
+app.get('/api/recipes', getRecipes)           // Get all recipes
+app.get('/api/recipes/:id', getRecipe)        // Get a specific recipe details
+app.delete('/api/recipes/:id', deleteRecipe)  // Delete a recipe
+app.put('/api/recipes/:id', updateRecipe)     // Update a recipe
+app.post('/api/recipes', createRecipe)        // Delete a recipe
 
-function getGame(req, resp){
-    resp.json({message : "getGameByID id = " + req.params.id })
-}
+app.get()
 
-function updateGame(req, resp){
-    console.log('Received in body: ', req.body)
-    resp.json({message : "updateGame id = " + req.params.id })
-    
-}
-
-function createGame(req, resp){
-    console.log('Received in body: ', req.body)
-    resp
-        .status(201)
-        .json({message : "createGame" })
-}
-
-function deleteGame(req, resp){ 
-    resp.json({message : "deleteGame id = " + req.params.id })
-}
